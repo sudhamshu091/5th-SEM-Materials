c3bbe0ba242aada90260458a14a04189947c44c6
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-09-14 13:21:12 +0530

    Add files via upload

---
+++ Python Programming/14-09-Assignment/bin_oct_hex.py
@@ -0,0 +1,6 @@
+def bin_oct_hex(n):
+    print("Binary equivalent of the decimal number is:",bin(n))
+    print("Octal equivalent of the decimal number is:",oct(n))
+    print("Hexadecimal equivalent of the decimal number is:",hex(n))
+bin_oct_hex(9)
+bin_oct_hex(16)
