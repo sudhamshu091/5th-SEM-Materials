716b31881c9b9c57db4895f85b736d661e34969e
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-09-14 16:01:24 +0530

    Update unpacked_bcd.py

--- Python Programming/14-09-Assignment/unpacked_bcd.py
+++ Python Programming/14-09-Assignment/unpacked_bcd.py
@@ -3,6 +3,7 @@ def unpacked_bcd(n):
     while(n > 0):
         num = n % 10;
         n = n // 10;
-        print(bin(num-1).replace("0b", "")+bin(num).replace("0b", ""))
+        print(bin(num-1).replace("0b", "0")+bin(num).replace("0b", "0"))    
 
 unpacked_bcd(4567)
+
