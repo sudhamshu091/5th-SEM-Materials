e999d7e4e254ffc277fe796f509fe9fc3328ebaf
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-09-28 10:35:24 +0530

    Update dff_dataflow.v

--- Digital System Design Using Verilog/28-09-Assignment/dff_dataflow.v
+++ Digital System Design Using Verilog/28-09-Assignment/dff_dataflow.v
@@ -1,1 +1,14 @@
+module dff_dataflow(
+    input clk,
+    input d,
+    output q
+    );
+wire w1,w2,q_n;
 
+assign w1 = ~(d & clk);
+assign w2 = ~(~d & clk);
+
+assign q = ~(w1 & q_n);
+assign q_n = ~(w2 & q);
+
+endmodule
