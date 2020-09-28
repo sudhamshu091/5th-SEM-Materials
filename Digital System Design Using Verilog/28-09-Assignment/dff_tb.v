ddb2b39c210e869b0867d686d5da5bfdbea8974b
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-09-28 10:51:54 +0530

    Create dff_tb.v

---
+++ Digital System Design Using Verilog/28-09-Assignment/dff_tb.v
@@ -0,0 +1,20 @@
+
+module dff_test;
+reg d, clk,reset;
+wire q, q_n;
+
+dff_n test(.q(q), .q_n(q_n), .clear(reset), .d(d), .clk(clk)); 
+
+$display("time = %g, clk = %b, d = %b,reset = %b, q = %b, q_n = %b", $time, clk, d, reset, q, q_n);
+
+initial begin
+  clk=0;
+     forever #10 clk = ~clk;  
+end 
+initial begin 
+ reset=1; d <= 0;
+ #48; reset=0; d <= 1;
+ #48; d <= 0;
+ #48; d <= 1;
+end 
+endmodule
