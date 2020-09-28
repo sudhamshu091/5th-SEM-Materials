d841df98081ed3088d8ca92cd9c9217e9be7ebb8
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-09-28 10:41:40 +0530

    Update dff_behavioral.v

--- Digital System Design Using Verilog/28-09-Assignment/dff_behavioral.v
+++ Digital System Design Using Verilog/28-09-Assignment/dff_behavioral.v
@@ -1,1 +1,13 @@
-
+module dff_behavioral(d,clk,clear,q,q_n); 
+input d, clk, clear; 
+output reg q, q_n; 
+always@(posedge clk) 
+begin
+if(clear== 1)
+q <= 0;
+q_n <= 1;
+else 
+q <= d; 
+q_n = !d; 
+end 
+endmodule
