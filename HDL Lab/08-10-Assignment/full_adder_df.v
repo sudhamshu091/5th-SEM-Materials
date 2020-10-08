c1170cc62740ec95de5ca9c14f5c167a3cb25809
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-10-08 17:21:07 +0530

    Add files via upload

---
+++ HDL Lab/08-10-Assignment/full_adder_df.v
@@ -0,0 +1,51 @@
+module fulladder (a, b, cin, sum, cout);
+input a, b, cin;
+output sum, cout;
+assign sum = a^b^cin;
+assign cout = (a&b)|(b&cin)|(a&cin);
+endmodule
+
+`timescale 1ns / 1ps
+module top;
+reg  a,b,cin;
+wire sum, cout;  
+fulladder adder(.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
+ initial begin
+ 
+ #100; 
+ 
+ a =1;
+ b <=0;
+ cin =0;
+ #10;
+ a =0;
+ b =1;
+ cin =0;
+ #10;
+ a=1;
+ b =1;
+ cin=0;
+ #10;
+ a =0;
+ b =0;
+ cin =1;
+ #10;
+ a =1;
+ b =0;
+ cin =1;
+ #10;
+ a =1;
+ b =0;
+ cin =1;
+ #10;
+ a =0;
+ b =1;
+ cin =1;
+ #10;
+ a=1;
+ b =1;
+ cin =1;
+ 
+ end
+      
+endmodule 
