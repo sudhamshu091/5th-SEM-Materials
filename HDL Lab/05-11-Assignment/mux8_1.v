cd0f62484359ebcce5ebc3133df90c0abde8e543
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-11-05 15:07:58 +0530

    Add files via upload

--- HDL Lab/05-11-Assignment/mux8_1.v
+++ HDL Lab/05-11-Assignment/mux8_1.v
@@ -1,42 +1,44 @@
-module mux8_1 (I, S, D);
-input [7:0] I;
+module demux1_8 (I, S, D);
+input I;
 input [2:0] S;
-output D;
-reg D;
+output [7:0]D;
+reg [7:0]D;
 always @ (I, S)
 begin
+D =8'b0;
  case(S)
- 3'd0: D=I[0];
- 3'd1: D=I[1];
- 3'd2: D=I[2];
- 3'd3: D=I[3];
- 3'd4: D=I[4];
- 3'd5: D=I[5];
- 3'd6: D=I[6];
- 3'd7: D=I[7];
-default: D=1'BZ;
+ 3'd0: D[0]=I;
+ 3'd1: D[1]=I;
+ 3'd2: D[2]=I;
+ 3'd3: D[3]=I;
+ 3'd4: D[4]=I;
+ 3'd5: D[5]=I;
+ 3'd6: D[6]=I;
+ 3'd7: D[7]=I;
+default: D=8'b00000000;
 endcase
 end
 endmodule
 
-module mux_tb;
 
-wire D;
+module demux1_8_tb;
+
+wire [7:0]D;
 
 reg [2:0]S;
-reg [7:0]I;
+reg I;
 
-mux8_1 mux( .D(D), .I(I), .S(S) );
+demux1_8 demux( .D(D), .I(I), .S(S) );
 
 initial begin
-$monitor(S,I,D);
 
 S=3'b000;
-I=8'b10111011;
+I=1'b1;
+#45 $finish;
 
 end 
 
-always #20 
-S=S+3'b001;
+always 
+ #6 S=S+3'b001;
 
 endmodule
