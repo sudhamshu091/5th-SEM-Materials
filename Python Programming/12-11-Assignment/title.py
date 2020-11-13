f54e7a80b62f1ab86b46082315dc0869717b2ac5
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-11-13 09:54:16 +0530

    Update title.py

--- Python Programming/12-11-Assignment/title.py
+++ Python Programming/12-11-Assignment/title.py
@@ -10,3 +10,7 @@ results = re.search("<title.*?>.*?</title.*?>", html, re.IGNORECASE)
 title = results.group()
 title = re.sub("<.*?>", "", title)
 print(title)
+
+#output
+#Program to extract title
+#Kasaragod - Wikipedia
