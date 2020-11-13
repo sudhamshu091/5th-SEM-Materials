6ee3aeb1d9bf04462bb7a6908db9fea53f41d728
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-11-13 09:53:17 +0530

    Add files via upload

--- Python Programming/12-11-Assignment/title.py
+++ Python Programming/12-11-Assignment/title.py
@@ -1,1 +1,12 @@
+print("Program to extract title")
+import re
+from urllib.request import urlopen
 
+url = "https://en.wikipedia.org/wiki/Kasaragod"
+open = urlopen(url)
+html = open.read().decode("utf-8")
+
+results = re.search("<title.*?>.*?</title.*?>", html, re.IGNORECASE)
+title = results.group()
+title = re.sub("<.*?>", "", title)
+print(title)
