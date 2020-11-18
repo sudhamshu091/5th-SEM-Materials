9f24fa068e7c9c71a890bbff5e189624151e17e8
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-11-18 12:14:16 +0530

    Add files via upload

---
+++ HDL Lab/Other experiments/bintogray.v
@@ -0,0 +1,40 @@
+module bin_to_gray(B,G);
+input [3:0] B;
+output [3:0] G;
+assign G[0] = B[1]^B[0];
+assign G[1] = B[2]^B[1];
+assign G[2] = B[3]^B[2];
+assign G[3] = B[3];
+endmodule
+
+module bin_to_gray_tb();
+
+   reg [3:0] G;
+    wire [3:0] B;
+    
+   bin_to_gray uut(G,B);
+    
+
+   always
+   begin        
+          G <= 0; #10;
+        G <= 1;   #10;
+        G <= 2;   #10;
+        G <= 3;   #10;
+        G <= 4;   #10;
+        G <= 5;   #10;
+        G <= 6;   #10;
+        G <= 7;   #10;
+        G <= 8;   #10;
+        G <= 9;   #10;
+        G <= 10;  #10;
+        G <= 11;  #10;
+        G <= 12;  #10;
+        G <= 13;  #10;
+        G <= 14;  #10;
+        G <= 15;  #10;
+        #100;   
+      $stop;
+   end
+
+endmodule
--- HDL Lab/Other experiments/comparator.v
+++ HDL Lab/Other experiments/comparator.v
@@ -1,1 +1,51 @@
+module comparator (a, b, alb, aeb, agb);
+input[3:0] a, b;
+output alb, aeb, agb;
+reg alb, aeb, agb;
+always@(a,b)
+begin
+if(a<b)
+begin
+alb=1'b1; aeb=1'b0; agb=1'b0;
+end
+else if (a>b)
+begin
+alb=1'b0; aeb=1'b0; agb=1'b1;
+end
+else
+begin
+alb=1'b0; aeb=1'b1; agb=1'b0;
+end
+end
+endmodule
 
+module comparator_tb;
+
+    reg [3:0] a;
+    reg [3:0] b;
+
+    wire alb;
+    wire aeb;
+    wire agb;
+
+    comparator uut (
+        .a(a), 
+        .b(b), 
+        .alb(alb), 
+        .aeb(aeb), 
+        .agb(agb)
+    );
+
+    initial begin
+        a = 10;
+        b = 12;
+        #100;
+        a = 15;
+        b = 11;
+        #100;
+        a = 10;
+        b = 10;
+        #100;
+    end
+      
+endmodule
