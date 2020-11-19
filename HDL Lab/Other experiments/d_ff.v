7106248411f2097566bc354e94617ee3720aa6b3
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-11-19 13:13:42 +0530

    Add files via upload

---
+++ HDL Lab/Other experiments/d_ff.v
@@ -0,0 +1,29 @@
+module dff(d, rst, clk, q, qb);
+ input d, rst, clk; 
+output q, qb; reg q, qb; 
+always @(posedge clk) begin if (rst == 1'B1) begin q = 1'B0;
+ qb = 1'B1;
+ end else begin q = d; 
+qb = ~q; 
+end 
+end 
+endmodule
+
+
+module dff_test;
+reg d, clk,rst;
+wire q, qb;
+
+dff dut(.q(q), .qb(qb), .rst(rst), .d(d), .clk(clk));
+
+initial begin
+  clk=0;
+     forever #10 clk = ~clk;  
+end 
+initial begin 
+ rst=1; d <= 0;
+ #100; rst=0; d <= 1;
+ #100; d <= 0;
+ #100; d <= 1;
+end 
+endmodule
