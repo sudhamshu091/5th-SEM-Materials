90f9d73084f92a5a1b64c3b3092e7b49849605fb
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-12-29 12:32:41 +0530

    Create jk_hardware.v

---
+++ HDL Lab/Other experiments/jk_hardware.v
@@ -0,0 +1,24 @@
+module jkff(jk, clk, q, qb);
+input clk;
+input [1:0]jk;
+output q,qb;
+reg q,qb;
+reg clk1;
+reg[22:0] div
+always @(posedge clk)
+begin
+div<=div+1’b1;
+clk1<=div[22];
+end
+always @(posedge clk1)
+begin
+case(jk)
+2'b00:q=q;
+2'b01:q=1'b0;
+2'b10:q=1'b1;
+2’b11:q= ~q;
+default:q=1'bz;
+endcase
+qb=~q;
+end
+endmodule
