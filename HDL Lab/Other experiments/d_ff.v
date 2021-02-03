e4d14f07936f65919bf67c2353c909fdd4a31295
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-02-03 19:55:23 +0530

    Update d_ff.v

--- HDL Lab/Other experiments/d_ff.v
+++ HDL Lab/Other experiments/d_ff.v
@@ -1,9 +1,16 @@
 module dff(d, rst, clk, q, qb);
  input d, rst, clk; 
 output q, qb; reg q, qb; 
-always @(posedge clk) begin if (rst == 1'B1) begin q = 1'B0;
- qb = 1'B1;
- end else begin q = d; 
+always @(posedge clk) 
+begin 
+if (rst == 1'B1) 
+begin 
+q = 1'B0;
+qb = 1'B1;
+end 
+else 
+begin 
+q = d; 
 qb = ~q; 
 end 
 end 
